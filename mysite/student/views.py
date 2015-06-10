c9aba3bb99ff2b99c9fe743b59e4331796d6f463
diaodiao <yuanpengxiong@foxmail.com>
2015-06-11 04:41:02 +0800

    测试

--- mysite/student/views.py
+++ mysite/student/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.Http import   HttpResponseRedirect
+from teacher.models import Paper,Score
 # Create your views here.
-
 def ViewPaper(request):
-    AuthError = False
-    if request.user.is_authenticated():
-        StudentId = request.user.name
-    else:
-        AuthError = True
-    return render_to_response('OnlineTest',locals())
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/user_auth/')
+    if request.method =='GET':
+        PaperIdList = Paper.objects.filter(ClassId = ClassID)
+        for paperid in PaperIdList:
+            P = Score.objects.filter(StudentId=request.user.name,ClassId=ClassID)
+            paperid['MaxScore'] = P.MaxScore
+            paperid['ValidScore'] = P.ValidScore
+            paperid['SubmitTime'] = P.SubmitTime
+            paperid['URL'] = request.get_host+'/paper/'+paperid.PaperId
+        return render_to_response('OnlineTest',{'PaperList':PaperIDList})
 
 def OnlinePaper(request):
     AuthError = False
@@ -19,4 +25,5 @@ def OnlinePaper(request):
         AuthError = True
 
 def ReturnScore(request):
+
     return render_to_response('',locals())
